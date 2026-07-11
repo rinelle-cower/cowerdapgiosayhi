421dcf8eb456cdec13b5a24f5b3357799993e97d
rinelle-cower <thesurvialshow@gmail.com>
2026-07-11 20:13:33 +0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,257 @@
+const database = [
+
+{
+code:"DG2026-001",
+name:"Orine",
+round1:"78.75",
+rank1:"10",
+cong1:"Thăng cấp",
+score1:"32",
+rankCong1:"1",
+cong2:"Thăng cấp",
+score2:"31",
+rankCong2:"11"
+},
+
+{
+code:"DG2026-002",
+name:"Nova",
+round1:"64.65",
+rank1:"17",
+cong1:"Thăng cấp",
+score1:"6",
+rankCong1:"13",
+cong2:"Rút lui",
+score2:"0",
+rankCong2:"15"
+},
+
+{
+code:"DG2026-003",
+name:"SUMMER",
+round1:"81.35",
+rank1:"5",
+cong1:"Thăng cấp",
+score1:"20",
+rankCong1:"2",
+cong2:"Loại",
+score2:"0",
+rankCong2:"15"
+},
+
+{
+code:"DG2026-004",
+name:"naf",
+round1:"81.50",
+rank1:"3",
+cong1:"Thăng cấp",
+score1:"19",
+rankCong1:"3",
+cong2:"Thăng cấp",
+score2:"52.5",
+rankCong2:"2"
+},
+
+{
+code:"DG2026-005",
+name:"anizuoe",
+round1:"63.75",
+rank1:"18",
+cong1:"Thăng cấp",
+score1:"8",
+rankCong1:"11",
+cong2:"Thăng cấp",
+score2:"14.5",
+rankCong2:"14"
+},
+
+{
+code:"DG2026-006",
+name:"clysia",
+round1:"79.5",
+rank1:"9",
+cong1:"Thăng cấp",
+score1:"16",
+rankCong1:"6",
+cong2:"Thăng cấp",
+score2:"45.75",
+rankCong2:"4"
+},
+
+{
+code:"DG2026-007",
+name:"Kami",
+round1:"75",
+rank1:"15",
+cong1:"Thăng cấp",
+score1:"3",
+rankCong1:"15",
+cong2:"Thăng cấp",
+score2:"21.5",
+rankCong2:"13"
+},
+
+{
+code:"DG2026-008",
+name:"m.",
+round1:"81",
+rank1:"6",
+cong1:"Thăng cấp",
+score1:"19",
+rankCong1:"3",
+cong2:"Thăng cấp",
+score2:"48.5",
+rankCong2:"3"
+},
+
+{
+code:"DG2026-009",
+name:"Suzy",
+round1:"76",
+rank1:"13",
+cong1:"Thăng cấp",
+score1:"1",
+rankCong1:"16",
+cong2:"Thăng cấp",
+score2:"36.25",
+rankCong2:"9"
+},
+
+{
+code:"DG2026-010",
+name:"Tuni",
+round1:"81.75",
+rank1:"2",
+cong1:"Thăng cấp",
+score1:"19",
+rankCong1:"3",
+cong2:"Thăng cấp",
+score2:"62.75",
+rankCong2:"1"
+},
+
+{
+code:"DG2026-011",
+name:"Jeyun",
+round1:"81.4",
+rank1:"4",
+cong1:"Rút lui",
+score1:"0",
+rankCong1:"18"
+},
+
+{
+code:"DG2026-012",
+name:"Velirez",
+round1:"-",
+rank1:"-",
+cong1:"Thăng cấp",
+score1:"-3",
+rankCong1:"17",
+cong2:"Loại",
+score2:"0",
+rankCong2:"15"
+},
+
+{
+code:"DG2026-013",
+name:"Nthyy",
+round1:"77.75",
+rank1:"11",
+cong1:"Thăng cấp",
+score1:"13",
+rankCong1:"7",
+cong2:"Thăng cấp",
+score2:"38.5",
+rankCong2:"8"
+},
+
+{
+code:"DG2026-014",
+name:"Zwie",
+round1:"9.5",
+rank1:"19",
+cong1:"Thăng cấp",
+score1:"8",
+rankCong1:"11",
+cong2:"Loại",
+score2:"0",
+rankCong2:"15"
+},
+
+{
+code:"DG2026-015",
+name:"Lina",
+round1:"80",
+rank1:"8",
+cong1:"Loại",
+score1:"-4",
+rankCong1:"19"
+},
+
+{
+code:"DG2026-016",
+name:"stuper",
+round1:"82",
+rank1:"1",
+cong1:"Thăng cấp",
+score1:"5",
+rankCong1:"14",
+cong2:"Thăng cấp",
+score2:"41.5",
+rankCong2:"7"
+},
+
+{
+code:"DG2026-017",
+name:"XeooRo",
+round1:"80.5",
+rank1:"7",
+cong1:"Thăng cấp",
+score1:"12",
+rankCong1:"8",
+cong2:"Thăng cấp",
+score2:"27",
+rankCong2:"12"
+},
+
+{
+code:"DG2026-018",
+name:"Eonix",
+round1:"77.75",
+rank1:"11",
+cong1:"Thăng cấp",
+score1:"17",
+rankCong1:"5",
+cong2:"Thăng cấp",
+score2:"42",
+rankCong2:"6"
+},
+
+{
+code:"DG2026-019",
+name:"zk",
+round1:"75.1",
+rank1:"14",
+cong1:"Thăng cấp",
+score1:"15",
+rankCong1:"9",
+cong2:"Thăng cấp",
+score2:"32",
+rankCong2:"10"
+},
+
+{
+code:"DG2026-020",
+name:"n",
+round1:"70.25",
+rank1:"16",
+cong1:"Thăng cấp",
+score1:"10",
+rankCong1:"10",
+cong2:"Thăng cấp",
+score2:"45.75",
+rankCong2:"4"
+}
+
+];
